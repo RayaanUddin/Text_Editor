d42a4c68615480997578d6af5ed6f284ca485f5f
Rayaan Uddin <rayaan.uddin@outlook.com>
2025-01-19 03:46:40 +0000

    Added core features, save, new, close, autosaving and more

--- src/TextEditorFrame.java
+++ src/TextEditorFrame.java
@@ -1,14 +1,23 @@
 import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.KeyEvent;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
+import javax.swing.filechooser.FileSystemView;
+import java.awt.*;
+import java.awt.event.*;
+import java.io.*;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.Scanner;
 
 public class TextEditorFrame extends JFrame {
     static Boolean isMacos = null;
-    public TextEditorFrame() {
+    final LinkedHashMap<String, Map<String, AbstractAction>> actionsCategory = new LinkedHashMap<>();
+    File file;
+    boolean needsUpdate = false;
+    private final JTextArea area = new JTextArea();
+    Thread autosaving;
+    public TextEditorFrame(File file) {
         if (isMacos == null) {
             if (System.getProperty("os.name").toLowerCase().contains("mac")) {
                 isMacos = true;
@@ -18,10 +27,13 @@ public class TextEditorFrame extends JFrame {
         }
         if (isMacos) {
             System.setProperty("apple.laf.useScreenMenuBar", "true");
-            System.setProperty("com.apple.mrj.application.apple.menu.about.name", "MyApp");
         }
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        int CTRL = isMacos ? KeyEvent.META_DOWN_MASK : KeyEvent.CTRL_DOWN_MASK;
+        setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
         setSize(500, 700);
+        setTitle("Untitled");
+        System.setProperty("name", "Text Editor");
+        setLocationRelativeTo(null);
 
         // Set Mnemonics (Alt+Key to open menus)
         // Create the menu bar
@@ -30,26 +42,49 @@ public class TextEditorFrame extends JFrame {
         // Define actions using a Map
         Map<String, AbstractAction> fileActions = new HashMap<>();
         Map<String, AbstractAction> editActions = new HashMap<>();
-        LinkedHashMap<String, Map<String, AbstractAction>> actionsCategory = new LinkedHashMap<>();
         actionsCategory.put("File", fileActions);
         actionsCategory.put("Edit", editActions);
 
         // Define menu actions with icons and keyboard shortcuts
         fileActions.put("New", new AbstractAction("New") {
             public void actionPerformed(ActionEvent e) {
-                new TextEditorFrame();
+                new TextEditorFrame(null);
+            }
+        });
+
+        addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                close();
             }
         });
 
         fileActions.put("Open", new AbstractAction("Open") {
             public void actionPerformed(ActionEvent e) {
-                System.out.println("File Opened");
+                JFileChooser jfc = new JFileChooser(FileSystemView.getFileSystemView().getHomeDirectory());
+                jfc.setDialogTitle("Choose destination.");
+                jfc.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
+                int returnValue = jfc.showOpenDialog(null);
+                if (returnValue == JFileChooser.APPROVE_OPTION) {
+                    File file = new File(jfc.getSelectedFile().getAbsolutePath());
+                    open(file);
+                }
             }
         });
 
-        fileActions.put("Exit", new AbstractAction("Exit") {
+        fileActions.put("Save", new AbstractAction("Save") {
             public void actionPerformed(ActionEvent e) {
-                System.exit(0);
+                save();
+            }
+        });
+
+        fileActions.put("SaveAs", new AbstractAction("Save As") {
+            public void actionPerformed(ActionEvent e) {
+                JFileChooser fileChooser = new JFileChooser();
+                if (fileChooser.showSaveDialog(null) == JFileChooser.APPROVE_OPTION) {
+                    File file = fileChooser.getSelectedFile();
+                    save(file);
+                }
             }
         });
 
@@ -72,12 +107,12 @@ public class TextEditorFrame extends JFrame {
         });
 
         // Add keyboard shortcuts (Accelerators)
-        fileActions.get("New").putValue(AbstractAction.ACCELERATOR_KEY, KeyStroke.getKeyStroke(KeyEvent.VK_N, KeyEvent.META_DOWN_MASK));  // ⌘ + N
-        fileActions.get("Open").putValue(AbstractAction.ACCELERATOR_KEY, KeyStroke.getKeyStroke(KeyEvent.VK_O, KeyEvent.META_DOWN_MASK)); // ⌘ + O
-        fileActions.get("Exit").putValue(AbstractAction.ACCELERATOR_KEY, KeyStroke.getKeyStroke(KeyEvent.VK_Q, KeyEvent.META_DOWN_MASK));  // ⌘ + Q
-        editActions.get("Cut").putValue(AbstractAction.ACCELERATOR_KEY, KeyStroke.getKeyStroke(KeyEvent.VK_X, KeyEvent.META_DOWN_MASK));   // ⌘ + X
-        editActions.get("Copy").putValue(AbstractAction.ACCELERATOR_KEY, KeyStroke.getKeyStroke(KeyEvent.VK_C, KeyEvent.META_DOWN_MASK));  // ⌘ + C
-        editActions.get("Paste").putValue(AbstractAction.ACCELERATOR_KEY, KeyStroke.getKeyStroke(KeyEvent.VK_V, KeyEvent.META_DOWN_MASK)); // ⌘ + V
+        fileActions.get("New").putValue(AbstractAction.ACCELERATOR_KEY, KeyStroke.getKeyStroke(KeyEvent.VK_N, CTRL));  // ⌘ + N
+        fileActions.get("Open").putValue(AbstractAction.ACCELERATOR_KEY, KeyStroke.getKeyStroke(KeyEvent.VK_O, CTRL)); // ⌘ + O
+        fileActions.get("Save").putValue(AbstractAction.ACCELERATOR_KEY, KeyStroke.getKeyStroke(KeyEvent.VK_S, CTRL));  // ⌘ + S
+        fileActions.get("SaveAs").putValue(AbstractAction.ACCELERATOR_KEY, KeyStroke.getKeyStroke(KeyEvent.VK_S, CTRL | KeyEvent.SHIFT_DOWN_MASK));  // ⌘ + SHIFT + S        editActions.get("Cut").putValue(AbstractAction.ACCELERATOR_KEY, KeyStroke.getKeyStroke(KeyEvent.VK_X, CTRL));   // ⌘ + X
+        editActions.get("Copy").putValue(AbstractAction.ACCELERATOR_KEY, KeyStroke.getKeyStroke(KeyEvent.VK_C, CTRL));  // ⌘ + C
+        editActions.get("Paste").putValue(AbstractAction.ACCELERATOR_KEY, KeyStroke.getKeyStroke(KeyEvent.VK_V, CTRL)); // ⌘ + V
 
         // Create and add menu items dynamically
         for (var category : actionsCategory.entrySet()) {
@@ -91,14 +126,129 @@ public class TextEditorFrame extends JFrame {
         // Set the menu bar for the frame
         setJMenuBar(menuBar);
 
+        // Update text editor
+        area.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
+        area.setMargin(new Insets(10, 5, 10, 5));
+        JScrollPane scrollPane = new JScrollPane(area);
+        scrollPane.setBorder(null);
+        add(scrollPane, BorderLayout.CENTER);
+
+        // Add DocumentListener to detect changes
+        area.getDocument().addDocumentListener(new DocumentListener() {
+            @Override
+            public void insertUpdate(DocumentEvent e) {
+                needsUpdate = true;
+            }
+
+            @Override
+            public void removeUpdate(DocumentEvent e) {
+                needsUpdate = true;
+            }
+
+            @Override
+            public void changedUpdate(DocumentEvent e) {
+                needsUpdate = true;
+            }
+        });
+
+        autosaving = new Thread(() -> {
+            while (true) {
+                try {
+                    Thread.sleep(10000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                if (needsUpdate) {
+                    save();
+                }
+            }
+        });
+
+        if (file != null) {
+            open(file);
+            autosaving.start();
+        } else {
+            fileActions.get("Save").setEnabled(false);
+        }
+
         // Make the frame visible
         setVisible(true);
+        toFront();
     }
 
+    public void close() {
+        if (file == null && !getText().trim().isEmpty()) {
+            int option = JOptionPane.showConfirmDialog(this, "You have unsaved changes. Do you want to save before exiting?", "Unsaved Changes", JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.WARNING_MESSAGE);
+            if (option == JOptionPane.YES_OPTION) {
+                save();
+                dispose();
+            } else if (option == JOptionPane.NO_OPTION) {
+                dispose(); // Close window without saving
+            }
+        } else {
+            if (autosaving.isAlive()) {
+                save();
+            }
+            dispose();
+        }
+    }
 
+    public void open(File file) {
+        if (getText().trim().isEmpty()) {
+            try {
+                StringBuilder text = new StringBuilder();
+                FileReader read = new FileReader(file);
+                Scanner scan = new Scanner(read);
+                while (scan.hasNextLine()) {
+                    String line = scan.nextLine() + "\n";
+                    text.append(line);
+                }
+                setText(text.toString());
+                this.file = file;
+                setTitle(file.getName());
+                actionsCategory.get("File").get("Save").setEnabled(true);
+            } catch (FileNotFoundException ignored) {
+            }
+        } else {
+            toBack();
+            new TextEditorFrame(file);
+        }
+    }
+
+    public void save() {
+        try {
+            File file = new File(this.file.getAbsolutePath());
+            Writer writer = new FileWriter(file);
+            writer.write(getText());
+            writer.close();
+            setTitle(file.getName());
+            if (!autosaving.isAlive()) {
+                autosaving.start();
+            }
+        } catch (Exception e) {
+            JFileChooser fileChooser = new JFileChooser();
+            if (fileChooser.showSaveDialog(null) == JFileChooser.APPROVE_OPTION) {
+                File file = fileChooser.getSelectedFile();
+                save(file);
+            }
+        }
+    }
+
+    public void save(File file) {
+        this.file = file;
+        save();
+    }
+
+    public void setText(String text) {
+        area.setText(text);
+    }
+
+    public String getText() {
+        return area.getText();
+    }
 
     // For testing purpose
     public static void main(String[] args) {
-        JFrame frame = new TextEditorFrame();
+        JFrame frame = new TextEditorFrame(null);
     }
 }
